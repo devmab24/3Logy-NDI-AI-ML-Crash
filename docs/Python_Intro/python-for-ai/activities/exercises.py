57611b9631eef153a68dd8a56bca8db540231e12
Muhammad Ayuba Bello <talk2muhammadbello@gmail.com>
2026-06-14 18:05:30 +0100

    Adding the python files

---
+++ docs/Python_Intro/python-for-ai/activities/exercises.py
@@ -0,0 +1,21 @@
+# Practice questions
+
+# 1. What is a function in Python?
+# 2. Why are functions useful in programming?
+# 3. How do you define a function in Python?
+# 4. What is the difference between a parameter and an argument?
+# 5. How do you call a function in Python?
+# 6. What is the purpose of the return statement in a function?
+# 7. What is the scope of a variable in Python?
+# 8. What is a docstring in Python?
+# 9. How do you create a function with default parameter values?
+# 10. What are keyword arguments in Python?
+
+# _______________________________________________________________
+# Task 1: Create a function called: calculate_area()` 
+# that takes the radius of a circle as a parameter and returns the area of the circle. 
+# Use the formula: area = π * radius^2. You can use the value of π as 3.14.`
+
+# Task 2:Create a function: `is_even()` 
+# that takes a number as a parameter and returns `True` if the number is even 
+# and `False` if the number is odd.
---
+++ docs/Python_Intro/python-for-ai/activities/mini_tasks.py
@@ -0,0 +1,7 @@
+"""
+1. Create functions for:
+    1. calculate_average()
+    2. assign_grade()
+    3. display_info()/display_report()
+    4. Submit on github
+"""
